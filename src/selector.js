8954411c6063e4c76ee85a4e299b8ff7baf6bb86
Radon4ik <Loboda.Radimir@gmail.com>
2021-12-27 01:44:33 +0300

    edit js file

--- src/selector.js
+++ src/selector.js
@@ -1,5 +1,31 @@
-const selector = (obj, keys) => {
-    // Ваше решение должно быть здесь
+const object = {
+    test: {
+        myField: {
+            'value string': 'result',
+        },
+    },
 };
 
+let a;
+
+const selector = (obj, keys) => {
+
+    let a;
+    
+
+
+    for (keys[a] in obj) {
+        return obj[keys[a]];
+
+
+    }
+
+};
+
+console.log(selector(object, ['test', 'myField', 'value string'])); // => 'result'
+console.log(selector(object, ['test', 'notExistField', 'value string'])); // => ''
+
+
+
 module.exports = selector;
+
