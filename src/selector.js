00d51d62d766e7d5d6e692046c8ee4cb2e1d11a1
Radon4ik <Loboda.Radimir@gmail.com>
2021-12-29 14:17:21 +0300

    edit js file

--- src/selector.js
+++ src/selector.js
@@ -6,21 +6,32 @@ const object = {
     },
 };
 
-let a;
 
 const selector = (obj, keys) => {
-
-    let a;
-    
-
-
-    for (keys[a] in obj) {
-        return obj[keys[a]];
+    // console.log('аргумент obj принимает Объект =', obj);
+    // console.log('аргумент keys принимает массив строк =', keys);
+    let a = obj;
+    // console.log('объект записывается в переменную а =', obj);
 
 
-    }
+    keys.forEach((item) => {
+        // console.log('массив строк попадает в foreach каждая строка попадает в (item)  =', item);
+        if (a[item]) {
+            a = a[item];
+            // console.log('a(объект)+[item](строка) =', a);
+        } else {
+            a = " ";
+            // console.log('если строки нет в объекте, то = " возвращает пустую строку "', a);
+        }
+    });
+    return a;
 
-};
+}
+
+
+
+
+
 
 console.log(selector(object, ['test', 'myField', 'value string'])); // => 'result'
 console.log(selector(object, ['test', 'notExistField', 'value string'])); // => ''
